c50dab27d7e13d091cac0d4c2512afa5194925bf
suraj <surajkumarbehera110@gmail.com>
2023-08-13 00:18:53 +0530

    remove particlejs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import './App.css';
 import { ExecutiveBody } from './component/ExecutiveBody';
 import { Gallery } from './component/Gallery';
-// import Particles from 'react-particles-js';
-// import ParticlesContainer from "./component/ParticlesContainer";
 
 export default function App() {
   return (
-    // <ParticlesContainer>
-      <div className=''>
-        {/* <Particles
-        params={{
-          particles: {
-            number: {
-              value: 200,
-              density: {
-                enable: true,
-                value_area: 1000,
-              }
-            },
-          },
-        }}
-      /> */}
+      <div >
         <div className='bg-blue-900 min-h-100vh '>
           <div className='pt-[4%] pl-[10%]'>
             <h2 className='text-2xl tracking-wide text-white font-julius'>GALLERY</h2>
@@ -45,6 +29,5 @@ export default function App() {
           </div>
         </div>
       </div>
-    // </ParticlesContainer>
   )
 }
